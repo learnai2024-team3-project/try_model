1c9a2df618ff42ceae4a6815b004f60c1d4ba2a8
Bill <billlin0904@gmail.com>
2024-08-09 15:26:41 +0800

    1. 修正keypoint 對應與數量錯誤 2. 增加其他1st model

--- kaggle_model.py
+++ kaggle_model.py
@@ -4,17 +4,34 @@ import numpy as np
 import time
 import json
 import tensorflow as tf
+import os
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 mp_hands = mp.solutions.hands
+mp_face_mesh = mp.solutions.face_mesh
 prevTime = 0
 
-# 加载 TensorFlow Lite 模型和字符映射
-interpreter = tf.lite.Interpreter("./Model/model.tflite")
+SRC_PATH = os.path.dirname(os.path.abspath(__file__))
 
-with open("./Model/character_to_prediction_index.json", "r") as f:
+BASE_PATH = "1st"
+INTEFFACE_ARGS_PATH = SRC_PATH + "/Model/" + BASE_PATH + "/inference_args.json"
+MODEL_PATH = SRC_PATH + "/Model/" + BASE_PATH + "/model.tflite"
+CHAR_MAP_PATH = SRC_PATH + "/Model/character_to_prediction_index.json"
+
+# 读取 JSON 文件中的特征
+with open(INTEFFACE_ARGS_PATH, 'r') as f:
+    inference_args = json.load(f)
+
+selected_columns = inference_args["selected_columns"]
+
+selected_columns_temp = inference_args.get("selected_columns", [])
+
+# 加载 TensorFlow Lite 模型和字符映射
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+
+with open(CHAR_MAP_PATH, "r") as f:
     character_map = json.load(f)
 rev_character_map = {j: i for i, j in character_map.items()}
 
@@ -31,79 +48,50 @@ if REQUIRED_SIGNATURE not in found_signatures:
 interpreter.allocate_tensors()
 
 # 获取推理函数
-prediction_fn = interpreter.get_signature_runner("serving_default")
+prediction_fn = interpreter.get_signature_runner(REQUIRED_SIGNATURE)
 
-# 定义用于ASL拼写识别的keyXYZ
-keyXYZ = [
-    "x_right_hand_0", "y_right_hand_0", "z_right_hand_0",
-    "x_right_hand_1", "y_right_hand_1", "z_right_hand_1",
-    "x_right_hand_2", "y_right_hand_2", "z_right_hand_2",
-    "x_right_hand_3", "y_right_hand_3", "z_right_hand_3",
-    "x_right_hand_4", "y_right_hand_4", "z_right_hand_4",
-    "x_right_hand_5", "y_right_hand_5", "z_right_hand_5",
-    "x_right_hand_6", "y_right_hand_6", "z_right_hand_6",
-    "x_right_hand_7", "y_right_hand_7", "z_right_hand_7",
-    "x_right_hand_8", "y_right_hand_8", "z_right_hand_8",
-    "x_right_hand_9", "y_right_hand_9", "z_right_hand_9",
-    "x_right_hand_10", "y_right_hand_10", "z_right_hand_10",
-    "x_right_hand_11", "y_right_hand_11", "z_right_hand_11",
-    "x_right_hand_12", "y_right_hand_12", "z_right_hand_12",
-    "x_right_hand_13", "y_right_hand_13", "z_right_hand_13",
-    "x_right_hand_14", "y_right_hand_14", "z_right_hand_14",
-    "x_right_hand_15", "y_right_hand_15", "z_right_hand_15",
-    "x_right_hand_16", "y_right_hand_16", "z_right_hand_16",
-    "x_right_hand_17", "y_right_hand_17", "z_right_hand_17",
-    "x_right_hand_18", "y_right_hand_18", "z_right_hand_18",
-    "x_right_hand_19", "y_right_hand_19", "z_right_hand_19",
-    "x_right_hand_20", "y_right_hand_20", "z_right_hand_20",
-    "x_left_hand_0", "y_left_hand_0", "z_left_hand_0",
-    "x_left_hand_1", "y_left_hand_1", "z_left_hand_1",
-    "x_left_hand_2", "y_left_hand_2", "z_left_hand_2",
-    "x_left_hand_3", "y_left_hand_3", "z_left_hand_3",
-    "x_left_hand_4", "y_left_hand_4", "z_left_hand_4",
-    "x_left_hand_5", "y_left_hand_5", "z_left_hand_5",
-    "x_left_hand_6", "y_left_hand_6", "z_left_hand_6",
-    "x_left_hand_7", "y_left_hand_7", "z_left_hand_7",
-    "x_left_hand_8", "y_left_hand_8", "z_left_hand_8",
-    "x_left_hand_9", "y_left_hand_9", "z_left_hand_9",
-    "x_left_hand_10", "y_left_hand_10", "z_left_hand_10",
-    "x_left_hand_11", "y_left_hand_11", "z_left_hand_11",
-    "x_left_hand_12", "y_left_hand_12", "z_left_hand_12",
-    "x_left_hand_13", "y_left_hand_13", "z_left_hand_13",
-    "x_left_hand_14", "y_left_hand_14", "z_left_hand_14",
-    "x_left_hand_15", "y_left_hand_15", "z_left_hand_15",
-    "x_left_hand_16", "y_left_hand_16", "z_left_hand_16",
-    "x_left_hand_17", "y_left_hand_17", "z_left_hand_17",
-    "x_left_hand_18", "y_left_hand_18", "z_left_hand_18",
-    "x_left_hand_19", "y_left_hand_19", "z_left_hand_19",
-    "x_left_hand_20", "y_left_hand_20", "z_left_hand_20",
-    "x_pose_13", "y_pose_13", "z_pose_13",
-    "x_pose_15", "y_pose_15", "z_pose_15",
-    "x_pose_17", "y_pose_17", "z_pose_17",
-    "x_pose_19", "y_pose_19", "z_pose_19",
-    "x_pose_21", "y_pose_21", "z_pose_21",
-    "x_pose_14", "y_pose_14", "z_pose_14",
-    "x_pose_16", "y_pose_16", "z_pose_16",
-    "x_pose_18", "y_pose_18", "z_pose_18",
-    "x_pose_20", "y_pose_20", "z_pose_20",
-    "x_pose_22", "y_pose_22", "z_pose_22"
-]
+# 打印 selected_columns 的长度
+print(len(selected_columns))  # 应该输出390
+res_point_size = len(selected_columns)
 
-# 确保总共156个数据点
-print(len(keyXYZ))  # 打印keyXYZ的长度
-print(len(keyXYZ) * 3)  # 打印keyXYZ的总数据点数
-
-# cap = cv2.VideoCapture("ABC.mp4")
 cap = cv2.VideoCapture(0)
+prediction_str = ""
 
-with mp_hands.Hands(
-        static_image_mode=False,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands, \
-     mp_pose.Pose(
-        static_image_mode=False,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
+def set_column_data(colum_name, idx, x, y, z):
+    name_x = f"x_{colum_name}_{idx}"
+    name_y = f"y_{colum_name}_{idx}"
+    name_z = f"z_{colum_name}_{idx}"
+    
+    for i, column in enumerate(column_lut):
+        if column[0] == name_x:
+            column_lut[i][1] = x
+            #print(f"set {column}")
+        elif column[0] == name_y:
+            column_lut[i][1] = y
+            #print(f"set {column}")
+        elif column[0] == name_z:
+            column_lut[i][1] = z
+            #print(f"set {column}")
+            
+# 直接使用 mp_drawing.DrawingSpec 来定义绘制风格
+face_landmark_style = mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
+face_connection_style = mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=1)
+
+hand_landmark_style = mp_drawing.DrawingSpec(color=(0, 255, 255), thickness=2, circle_radius=2)
+hand_connection_style = mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
+
+pose_landmark_style = mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
+pose_connection_style = mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
+
+
+with mp_face_mesh.FaceMesh(max_num_faces=1) as face_mesh, \
+     mp_hands.Hands(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands, \
+     mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    
+    column_lut = []
+    for column in selected_columns:
+        column_lut.append([column, 0])
+    
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -112,71 +100,92 @@ with mp_hands.Hands(
 
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        face_results = face_mesh.process(image)
         hand_results = hands.process(image)
         pose_results = pose.process(image)
 
-        res_point = []  # 清空 res_point
-        hand_points = []  # 用于存储手部关键点以绘制
-        pose_points = []  # 用于存储姿势关键点以绘制
+        res_point = []
 
-        if hand_results.multi_hand_landmarks:
-            for hand_landmarks in hand_results.multi_hand_landmarks:
-                for idx, landmark in enumerate(hand_landmarks.landmark):
-                    res_point.append(landmark.x)
-                    res_point.append(landmark.y)
-                    res_point.append(landmark.z)
-                    hand_points.append((landmark.x, landmark.y))
-
-        if pose_results.pose_landmarks:
-            for idx in [13, 15, 17, 19, 21, 14, 16, 18, 20, 22]:  # 获取与手部相关的姿势标志点
-                landmark = pose_results.pose_landmarks.landmark[idx]
-                res_point.append(landmark.x)
-                res_point.append(landmark.y)
-                res_point.append(landmark.z)
-                pose_points.append((landmark.x, landmark.y))
+        # 填充脸部数据
+        if face_results.multi_face_landmarks:
+            for face_landmarks in face_results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=face_landmark_style,
+                    connection_drawing_spec=face_connection_style)
                 
-        prediction_str = ""
-
-        # 确保res_point的长度为156
-        if len(res_point) == 156:
-            res_point = np.array(res_point).reshape(1, len(res_point)).astype(np.float32)
+                for idx, landmark in enumerate(face_landmarks.landmark):
+                    set_column_data("face", idx,
+                                    landmark.x,
+                                    landmark.y,
+                                    landmark.z)
+                
+        # 填充左手数据
+        # 填充右手数据
+        if hand_results.multi_hand_landmarks:
+            for hand_landmarks, handedness in zip(hand_results.multi_hand_landmarks, hand_results.multi_handedness):
+                hand_label = handedness.classification[0].label
+                
+                if hand_label == "Left":
+                    hand_name = "left_hand"
+                else:
+                    hand_name = "right_hand"
             
+                for idx, landmark in enumerate(hand_landmarks.landmark):
+                    set_column_data(hand_name, idx,
+                                    landmark.x,
+                                    landmark.y,
+                                    landmark.z)
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=hand_landmarks,
+                    connections=mp_hands.HAND_CONNECTIONS,
+                    landmark_drawing_spec=hand_landmark_style,
+                    connection_drawing_spec=hand_connection_style)
+        
+        # 填充姿态数据
+        if pose_results.pose_landmarks:
+            for idx, landmark in enumerate(pose_results.pose_landmarks.landmark):
+                set_column_data("pose", idx,
+                                    landmark.x,
+                                    landmark.y,
+                                    landmark.z)
+            mp_drawing.draw_landmarks(
+                image=image,
+                landmark_list=pose_results.pose_landmarks,
+                connections=mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=pose_landmark_style,
+                connection_drawing_spec=pose_connection_style)
+                
+        #print(column_lut)
+        
+        for i, column in enumerate(column_lut):
+            res_point.append(column_lut[i][1])
+
+        print(len(res_point))
+        
+        # 确保res_point的长度为res_point_size
+        if len(res_point) == res_point_size:
+            res_point = np.array(res_point).reshape(1, len(res_point)).astype(np.float32)
             # 执行预测
             output = prediction_fn(inputs=res_point)
-            prediction_str = "".join([rev_character_map.get(s, "") for s in np.argmax(output[REQUIRED_OUTPUT], axis=1)])
-            print(f"Prediction: {prediction_str}")
-
-            cv2.putText(image, f"Prediction: {prediction_str}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+            max_indices = np.argmax(output[REQUIRED_OUTPUT], axis=1)
+            prediction_str = "-".join([rev_character_map.get(s, "") for s in max_indices])
+            #prediction_str = rev_character_map.get(max_indices[0], "")
+        else:
+            prediction_str = "Data no enough"
+            
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # 绘制手部关键点（绿色）
-        h, w, _ = image.shape
-        for point in hand_points:
-            x = int(point[0] * w)
-            y = int(point[1] * h)
-            cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
-
-        # 绘制姿势关键点（红色）
-        for point in pose_points:
-            x = int(point[0] * w)
-            y = int(point[1] * h)
-            cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
-
-        # 将识别结构绘制在右上角
-        overlay = image.copy()
-        cv2.rectangle(overlay, (0, 0), (300, 100), (255, 255, 255), -1)
-        alpha = 0.6  # 透明度
-        image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-        cv2.putText(image, f"Prediction: {prediction_str}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
         currTime = time.time()
         fps = 1 / (currTime - prevTime)
         prevTime = currTime
-        cv2.putText(image, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 196, 255), 2)
 
-        cv2.imshow('MediaPipe Hands and Pose', image)
+        cv2.putText(image, f"Prediction: {prediction_str}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('MediaPipe Hands, Pose, and Face', image)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
